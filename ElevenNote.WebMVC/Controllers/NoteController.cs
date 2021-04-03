a41a7923b906029ca58734b1cae00fd01a81ba70
anicodesit <ani.kulka@gmail.com>
2021-04-03 13:42:14 -0400

    Added connection strings for Platform, Inventory, sales. Added Platform Model, View, and Controller

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,24 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note/Index
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
